bcd382cb29e127ca29bba8ad8271960bef1cdcd5
maxg56 <mgendrot56@gmail.com>
2025-06-03 13:17:24 +0200

    fix(graph): replace 'size' with 'node_count' for consistency in graph functions

--- tests/test_parsing.c
+++ tests/test_parsing.c
@@ -11,6 +11,8 @@
 /* ************************************************************************** */
 
 #include "test_lem_in.h"
+#include <stdio.h>  // Pour snprintf
+#include <stdlib.h> // Pour atoi
 
 void test_parse_rooms(void)
 {
@@ -21,13 +23,30 @@ void test_parse_rooms(void)
     
     print_test_result("Test map created", create_test_map_file("test_rooms.txt", test_map));
     
-    // TODO: When you implement parsing functions, add tests here
-    // Example:
-    // Graph* graph = parse_map_file("test_rooms.txt");
-    // print_test_result("Map parsed successfully", graph != NULL);
-    // print_test_result("Correct number of rooms", graph && graph->size == 4);
+    // Test actual parsing
+    Graph* graph = parse_from_file("test_rooms.txt");
+    print_test_result("Map parsed successfully", graph != NULL);
     
-    printf("    " TEST_INFO " Room parsing tests will be implemented when parsing functions are ready\n");
+    if (graph) {
+        print_test_result("Correct number of rooms", graph->node_count == 4);
+        print_test_result("Correct ant count", graph->nb_fourmis == 3);
+        
+        // Test start and end nodes
+        Node* start = getStartNode(graph);
+        Node* end = getEndNode(graph);
+        print_test_result("Start node found", start != NULL);
+        print_test_result("End node found", end != NULL);
+        
+        if (start) {
+            print_test_result("Start node name correct", ft_strcmp(start->Nan, "start") == 0);
+            print_test_result("Start node coordinates", start->x == 0 && start->y == 0);
+        }
+        
+        if (end) {
+            print_test_result("End node name correct", ft_strcmp(end->Nan, "end") == 0);
+            print_test_result("End node coordinates", end->x == 15 && end->y == 15);
+        }
+    }
     
     unlink("test_rooms.txt");
     ft_arna_free();
@@ -41,8 +60,29 @@ void test_parse_links(void)
     
     print_test_result("Test map with links created", create_test_map_file("test_links.txt", test_map));
     
-    // TODO: Add link parsing tests when implemented
-    printf("    " TEST_INFO " Link parsing tests will be implemented when parsing functions are ready\n");
+    // Test actual parsing with connections
+    Graph* graph = parse_from_file("test_links.txt");
+    print_test_result("Map with links parsed successfully", graph != NULL);
+    
+    if (graph) {
+        print_test_result("Correct number of rooms", graph->node_count == 3);
+        print_test_result("Correct ant count", graph->nb_fourmis == 3);
+        
+        // Test connections
+        int startIdx = findNodeByName(graph, "start");
+        int middleIdx = findNodeByName(graph, "middle");
+        int endIdx = findNodeByName(graph, "end");
+        
+        print_test_result("Start node found by name", startIdx != -1);
+        print_test_result("Middle node found by name", middleIdx != -1);
+        print_test_result("End node found by name", endIdx != -1);
+        
+        if (startIdx != -1 && middleIdx != -1 && endIdx != -1) {
+            print_test_result("start-middle connection exists", hasEdge(graph, startIdx, middleIdx));
+            print_test_result("middle-end connection exists", hasEdge(graph, middleIdx, endIdx));
+            print_test_result("No direct start-end connection", !hasEdge(graph, startIdx, endIdx));
+        }
+    }
     
     unlink("test_links.txt");
     ft_arna_free();
@@ -58,15 +98,42 @@ void test_parse_ants(void)
     printf("    " TEST_INFO " Testing valid ant counts:\n");
     for (int i = 0; i < 4; i++) {
         printf("        Valid: %s\n", valid_ant_counts[i]);
+        
+        // Create test map with this ant count
+        char test_map[100];
+        snprintf(test_map, sizeof(test_map), "%s\n##start\nstart 0 0\n##end\nend 5 5\nstart-end\n", valid_ant_counts[i]);
+        
+        char filename[50];
+        snprintf(filename, sizeof(filename), "test_ants_%d.txt", i);
+        
+        if (create_test_map_file(filename, test_map)) {
+            Graph* graph = parse_from_file(filename);
+            if (graph) {
+                int expected_ants = atoi(valid_ant_counts[i]);
+                print_test_result("Valid ant count parsed correctly", graph->nb_fourmis == expected_ants);
+            }
+            unlink(filename);
+        }
     }
     
     printf("    " TEST_INFO " Testing invalid ant counts:\n");
     for (int i = 0; i < 5; i++) {
         printf("        Invalid: %s\n", invalid_ant_counts[i]);
+        
+        // Create test map with invalid ant count
+        char test_map[100];
+        snprintf(test_map, sizeof(test_map), "%s\n##start\nstart 0 0\n##end\nend 5 5\nstart-end\n", invalid_ant_counts[i]);
+        
+        char filename[50];
+        snprintf(filename, sizeof(filename), "test_invalid_ants_%d.txt", i);
+        
+        if (create_test_map_file(filename, test_map)) {
+            Graph* graph = parse_from_file(filename);
+            // For invalid counts, either parsing should fail or ant count should be 0
+            print_test_result("Invalid ant count handled", !graph || graph->nb_fourmis == 0);
+            unlink(filename);
+        }
     }
-    
-    // TODO: Add actual parsing tests when implemented
-    printf("    " TEST_INFO " Ant parsing tests will be implemented when parsing functions are ready\n");
     
     ft_arna_free();
 }
